40b6e774b9a6255e5e7dcf02e4d9b4e5649fcac1
Sriya77 <sriyachakinala.com>
2025-04-13 12:01:13 +0530

    Implemented authorization and authentication

---
+++ web/backend/src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  connectDB();
+});
